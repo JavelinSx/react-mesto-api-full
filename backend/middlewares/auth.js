e604719d45540c169ce174949f10d6979a5f70b0
JavelinSx <zuev499@gmail.com>
2022-11-21 10:09:55 +0300

    test jwt env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 const BadAuthError = require('../errors/bad_auth');
+const { TOKEN_DEV } = require('../utils/const');
+require('dotenv').config();
 
 module.exports.auth = (req, res, next) => {
   const { token } = req.cookies;
-  const { JWT_SECRET } = process.env;
   let payload;
+  const { NODE_ENV = 'development', JWT_SECRET = 'some-secret-key' } = process.env;
   try {
-    payload = jwt.verify(token, JWT_SECRET);
-
-    console.log(payload);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : TOKEN_DEV);
   } catch (err) {
     next(new BadAuthError('Необходима авторизация'));
   }
--- backend/utils/const.js
+++ backend/utils/const.js
@@ -1,7 +1,5 @@
-const TOKEN_DEV = 'some-secret-key';
 const LINK_REGEX = /^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/;
 
 module.exports = {
   LINK_REGEX,
-  TOKEN_DEV,
 };
