c6cd269b30f1cbe66653593421717731aa0f0aff
JavelinSx <zuev499@gmail.com>
2022-11-21 09:13:13 +0300

    test jwt env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,13 +3,16 @@ const BadAuthError = require('../errors/bad_auth');
 const { TOKEN_DEV } = require('../utils/const');
 require('dotenv').config();
 
-const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports.auth = (req, res, next) => {
   const { token } = req.cookies;
+  const { NODE_ENV = 'development', JWT_SECRET = TOKEN_DEV } = process.env;
   let payload;
 
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : TOKEN_DEV);
+    console.log(JWT_SECRET);
+    console.log(TOKEN_DEV);
+    console.log(payload);
   } catch (err) {
     next(new BadAuthError('Необходима авторизация'));
   }
