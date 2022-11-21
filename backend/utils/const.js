3c472ad714b202bb80cf7cee5e3709613d37a957
JavelinSx <zuev499@gmail.com>
2022-11-21 12:27:37 +0300

    test env

--- backend/utils/const.js
+++ backend/utils/const.js
@@ -1,5 +1,7 @@
+const TOKEN_DEV = 'some-secret-key';
 const LINK_REGEX = /^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/;
 
 module.exports = {
   LINK_REGEX,
+  TOKEN_DEV,
 };
